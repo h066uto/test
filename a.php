6d71efe6c55eb855f17ca324b73e50666c48bcd3
h066uto <h520066u@mails.cc.ehime-u.ac.jp>
2022-07-11 15:24:23 +0900

    更新

--- a.php
+++ a.php
@@ -1,7 +1,9 @@
 #!/usr/bin/php
 <?php 
   foreach(file('/usr/share/dict/linux.words') as $line){
-	 
+if(strpos($line, $abc)){
+     echo "$line";
+   }	 
 	
        	// strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
   } 
